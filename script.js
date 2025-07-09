4c7bd8892b4b6ebd9b1c3d40f40329cd2022f5ea
KIRKISLAS <kirkislas96@gmail.com>
2025-07-09 15:11:01 -0700

    311

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleMenu() {
   const menu = document.getElementById("menu");
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
+  menu.classList.toggle("show");
 }
 
 function togglePlay() {
